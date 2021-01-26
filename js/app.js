9b5fe6566a3a648941d5845c3145e1392b159357
BernardaP <btp210@gmail.com>
2021-01-25 17:45:27 -0700

    object-ception

--- js/app.js
+++ js/app.js
@@ -143,3 +143,31 @@ console.log(crazyObject.larry.nicknames[1]);
 
 // The object that contains the name funkhauser
 console.log(crazyObject.larry.characters[1].name);
+
+console.log("---------------------------");
+console.log("-------------Q4------------");
+console.log("---------------------------");
+
+// Object-ception
+// With the following object:
+const inception = {
+    reality: {
+        dreamLayer1: {
+            dreamLayer2: {
+                dreamLayer3: {
+                    dreamLayer4: {
+                        dreamLayer5: {
+                            dreamLayer6: {
+                                limbo: "Joseph Gordon Levitt"
+                           }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Change the value of limbo to null.
+// console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo)
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo =null)
